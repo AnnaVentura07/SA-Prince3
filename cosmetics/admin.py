07c882c85b5d47f9388a69e1528d91f358e23b91
AnnaVentura07 <annaluisaventura07@gmail.com>
2026-04-16 20:38:00 -0300

    Remove Ingrediente from admin registration

--- cosmetics/admin.py
+++ cosmetics/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Categoria, Dica, Estoque, Produto, Ingrediente
+from .models import Categoria, Dica, Estoque, Produto
 
 admin.site.register(Categoria)
 admin.site.register(Produto)
-admin.site.register(Ingrediente)
 admin.site.register(Estoque)
 admin.site.register(Dica)
